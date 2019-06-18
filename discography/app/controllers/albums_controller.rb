125795b932232a5d9613392c0edd23fc08c765f0
Taylor <rapchak@gmail.com>
2019-06-18 14:27:54 -0400

    Now with Pictures!
    Saves album covers in a separate cover and shows them.

--- discography/app/controllers/albums_controller.rb
+++ discography/app/controllers/albums_controller.rb
@@ -10,6 +10,7 @@ class AlbumsController < ApplicationController
   # GET /albums/1
   # GET /albums/1.json
   def show
+    @album = Album.find(params[:id])
   end
 
   # GET /albums/new
@@ -69,6 +70,6 @@ class AlbumsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
-      params.require(:album).permit(:artist, :title, :id)
+      params.require(:album).permit(:artist, :title, :id, :cover)
     end
 end
