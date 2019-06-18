4e4d8067adbd644bf1afcb1a6dc6ba3e68c911e2
dasha_bo_or <sonja.rosenfeld@gmail.com>
2019-06-18 10:47:38 -0400

    Revert "Commit"
    This reverts commit 5a76a0fe8ecb30cd4f4d74c166e044da43d9ae3a.

--- discography/test/controllers/music_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MusicControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get music_index_url
-    assert_response :success
-  end
-
-end
